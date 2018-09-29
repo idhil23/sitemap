3756514ff92362cff8b86463a7fd6ce2956eea37
idhil23 <trick.gm@gmail.com>
2018-09-30 02:02:03 +0700

    Update ping.php

--- ping.php
+++ ping.php
@@ -1,4 +1,41 @@
 <?php
-$ping = file_get_contents('http://www.google.com/webmasters/sitemaps/ping?sitemap=http://google.com');
-echo $ping;
+set_time_limit(999999);
+$sitemaps = array(
+  "https://example.com/sitemap.xml",
+);
+// cUrl handler to ping the Sitemap submission URLs for Search Engines…
+function Submit($url){
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_HEADER, 0);
+  curl_exec($ch);
+  $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+  curl_close($ch);
+  return $httpCode;
+}
+function SubmitSiteMap($url) {
+  $returnCode = Submit($url);
+  if ($returnCode != 200) {
+    echo "Error $returnCode: $url <BR/>";
+  } else {
+    echo "Submitted $returnCode: $url <BR/>";
+  }
+}
+foreach ($sitemaps as $sitemapUrl) {
+  $sitemapUrl = htmlentities($sitemapUrl);
+  //Google  
+  $url = "http://www.google.com/webmasters/sitemaps/ping?sitemap=".$sitemapUrl;
+  SubmitSiteMap($url);
+  
+  //Bing / MSN
+  $url = "http://www.bing.com/webmaster/ping.aspx?siteMap=".$sitemapUrl;
+  SubmitSiteMap($url);
+  
+  // Live
+  $url = "http://webmaster.live.com/ping.aspx?siteMap=".$sitemapUrl;
+  SubmitSiteMap($url);
+  
+  // moreover
+  $url = "http://api.moreover.com/ping?sitemap=".$sitemapUrl;
+  SubmitSiteMap($url);
+}
 ?>
