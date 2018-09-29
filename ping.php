408d7f826454add5bb2667499c4e90a24b8c1453
idhil23 <trick.gm@gmail.com>
2018-09-30 00:53:13 +0700

    Update ping.php

--- ping.php
+++ ping.php
@@ -1,4 +1,4 @@
 <?php
-$ping = file_get_contents(http://www.google.com/webmasters/sitemaps/ping?sitemap=http://google.com);
+$ping = file_get_contents('http://www.google.com/webmasters/sitemaps/ping?sitemap=http://google.com');
 echo $ping;
 ?>
